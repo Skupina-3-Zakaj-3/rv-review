13145e0592908e942f7bd2a49a9ce80870f8151e
Tadej Stanonik <ts6103@student.uni-lj.si>
2022-01-02 21:27:18 +0100

    Service discovery and consul

--- api/src/main/java/si/fri/rso/skupina3/rv_review/v1/RvReviewApplication.java
+++ api/src/main/java/si/fri/rso/skupina3/rv_review/v1/RvReviewApplication.java
@@ -1,8 +1,11 @@
 package si.fri.rso.skupina3.rv_review.v1;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/v1")
+@RegisterService(value="rv-review-service", environment="dev", version = "1.0.0")
 public class RvReviewApplication extends Application {
 }
